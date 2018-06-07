2c4dade75320cb6e19a7ff47f74b4914ee3aaa66
mummy <mummy@mummys-Mac.local>
2018-06-07 02:29:25 -0700

    chatbot for altcoin

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+        <title>Chatbot For Altcoin</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <iframe
+            class="support-bot"
+            src="https://console.dialogflow.com/api-client/demo/embedded/9815312b-f6d9-449a-af69-081f2b63be54">
+        </iframe>
+        <!-- <iframe
+            width="350"
+            height="430"
+            src="https://console.dialogflow.com/api-client/demo/embedded/7c030dc0-e07c-49e0-b51e-d332ac8a805b">
+        </iframe> -->
+    </body>
+</html>
